3c82e384f832b107efa5632811be7ac5d463041a
Modupe <160359972+Modoop@users.noreply.github.com>
2024-05-17 18:25:27 -0400

    Update script.sh

--- modules/ec2/script.sh
+++ modules/ec2/script.sh
@@ -2,5 +2,5 @@
 sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
-systemctl enable httpd
+sudo systemctl enable httpd
 echo "<h1>This message is from : $(hostname -i)</h1>" > /var/www/html/index.html
