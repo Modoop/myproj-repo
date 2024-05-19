8e960832adca2bdc4d5d4c6f2e6a1e65d585cde9
Modupe Thomas <modupethomas21@gmail.com>
2024-05-19 18:46:40 -0400

    changes

--- provider.tf
+++ provider.tf
@@ -8,23 +8,23 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "Bucket Name"
+resource "aws_s3_bucket" "test_state" {
+  bucket        = var.bucket_name
   force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = aws_s3_bucket.test_state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket = aws_s3_bucket.terraform_state.bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "test_state_crypto_conf" {
+  bucket = aws_s3_bucket.test_state.bucket
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -32,21 +32,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
   }
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "Table Name"
+resource "aws_dynamodb_table" "test_table" {
+  name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
     name = "LockID"
     type = "S"
-  }
-}
-
-terraform {
-  backend "s3" {
-    bucket         = "Bucket Name"
-    key            = "path/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "Table Name"
   }
 }
